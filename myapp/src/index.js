65edbac3a154c286e0cceae271ebb70f487368de
developer <boris-plus@mail.ru>
2018-12-10 03:20:33 +0300

    ReduxApp extend React

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import { render } from "react-dom";
+import {Provider, connect} from 'react-redux';
+import store from "./redux/store/index";
+import App from "./components/App";
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const ReduxApp = connect( state => ({counter: state.counter}), dispatch => ({}))(App);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ReduxApp />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
