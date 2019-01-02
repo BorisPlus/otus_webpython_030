ded207ee9ecb6cccbbe82f8ccc6611f428262892
developer <boris-plus@mail.ru>
2019-01-02 14:22:36 +0300

    Change list: * css sideNav without tags names

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -4,11 +4,8 @@ import store from "./redux/store/index";
 import ReduxApp from "./redux/components/App";
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-//import './index.css';
 import './static/css/style.css';
-//import './static/css/min.css';
 import './static/css/sidenav.css';
-import './static/js/sidenav.js';
 import './static/css/forms.css';
 
 ReactDOM.render(
